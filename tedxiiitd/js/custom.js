3db6cf9b6c5d2c4ea3838f2ed46e46a170e8c6f5
anmoliiitd823 <32006941+anmoliiitd823@users.noreply.github.com>
2017-10-17 20:10:57 +0530

    Add files via upload

--- tedxiiitd/js/custom.js
+++ tedxiiitd/js/custom.js
@@ -19,3 +19,19 @@ $(function(){
 	});
 
 });
+$(function(){
+	$("#team-members").owlCarousel({
+		items: 3,
+		autoplay: true,
+		smartSpeed: 1000,
+		loop: true
+	}); 
+});
+$(function(){
+	$("#customers-past-speakers").owlCarousel({
+		items: 1,
+		autoplay: true,
+		smartSpeed: 1000,
+		loop: true
+	}); 
+});
